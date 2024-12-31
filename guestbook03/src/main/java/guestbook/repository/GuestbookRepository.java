2c0811993c4854b1b97e3b5f25ab3a0257645ac5
bakukun <h1101201@gmail.com>
2024-12-31 14:36:55 +0900

    Repository Refactoring #2

--- guestbook03/src/main/java/guestbook/repository/GuestbookRepository.java
+++ guestbook03/src/main/java/guestbook/repository/GuestbookRepository.java
@@ -1,23 +1,20 @@
 package guestbook.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.RowMapper;
 
-import guestbook.repository.template.StatementStrategy;
 import guestbook.repository.template.JdbcContext;
 import guestbook.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
+	
 	private JdbcContext jdbcContext;
 	private DataSource dataSource;
 
@@ -27,50 +24,26 @@ public class GuestbookRepository {
 	}
 
 	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> result = new ArrayList<>();
-
-		try (Connection conn = dataSource.getConnection();
-
-				PreparedStatement pstmt = conn.prepareStatement(
-						"select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc");
-				ResultSet rs = pstmt.executeQuery();) {
-			while (rs.next()) {
-				Long id = rs.getLong(1);
-				String name = rs.getString(2);
-				String contents = rs.getString(3);
-				String regDate = rs.getString(4);
-
+		return jdbcContext.queryForList(
+		"select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc",
+		new RowMapper<GuestbookVo>() {
+			@Override
+			public GuestbookVo mapRow(ResultSet rs, int rowNum) throws SQLException {
 				GuestbookVo vo = new GuestbookVo();
-				vo.setId(id);
-				vo.setName(name);
-				vo.setContents(contents);
-				vo.setRegDate(regDate);
-
-				result.add(vo);
-			}
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-
-		return result;
+				
+				vo.setId(rs.getLong(1));
+				vo.setName(rs.getString(2));
+				vo.setContents(rs.getString(3));
+				vo.setRegDate(rs.getString(4));
+				
+				return vo;
+			}	
+		});
 	}
 
 	public int insert(GuestbookVo vo) {
 		return jdbcContext.executeUpdate("insert into guestbook values(null, ?, ?, ?, now())",
 				new Object[] { vo.getName(), vo.getPassword(), vo.getContents() });
-		/*
-		 * int count = 0;
-		 * 
-		 * try ( Connection conn = getConnection(); PreparedStatement pstmt =
-		 * conn.prepareStatement("insert into guestbook values(null, ?, ?, ?, now())");
-		 * ) { pstmt.setString(1, vo.getName()); pstmt.setString(2, vo.getPassword());
-		 * pstmt.setString(3, vo.getContents());
-		 * 
-		 * count = pstmt.executeUpdate(); } catch (SQLException e) {
-		 * System.out.println("error:" + e); }
-		 * 
-		 * return count;
-		 */
 	}
 
 	public int deleteByIdAndPassword(Long id, String password) {
